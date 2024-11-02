d0655b7c2ce3796e1f588d18c45752076c6e8540
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-02 20:41:40 +0100

    mockdb for feature favourite route

---
+++ src/features/routeDB/mockdb.ts
@@ -0,0 +1,13 @@
+export type FavouriteRoutes = {
+  from: string;
+  to: string;
+  travelTime: number;
+};
+
+export const favouriteRoutes: FavouriteRoutes[] = [
+  {
+    from: "Bagarmossen",
+    to: "Fridhemsplan",
+    travelTime: 30,
+  },
+];
