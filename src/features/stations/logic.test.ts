506ab25798a0cc50eb31190be2d75dde455fc93c
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-02 15:36:37 +0100

    added supertest

--- src/features/stations/logic.test.ts
+++ src/features/stations/logic.test.ts
@@ -7,8 +7,10 @@
 //ValidateStations
 
 import test, { describe } from "node:test";
-import { subwayStations } from "../../stationDB/mockdb";
-import { validateStations } from "./logic";
+import { Station, subwayStations } from "../../stationDB/mockdb";
+import { calculateTravelTime, getTravelTime, validateStations } from "./logic";
+import request from "supertest";
+import { createApp } from "../../app";
 
 describe("validation ", () => {
   test.skip("travel between the same station should return: pick two diffrent stations", () => {
@@ -34,5 +36,41 @@ describe("validation ", () => {
     expect(result).toEqual({
       message: "Try another spelling of the stations or try diffrent stations",
     });
+  });
+  test.skip("when index diffrens is more then one, return: the stations needs to be located after eachother", () => {
+    const data = {
+      locationId: "FRIDHEMSPLAN",
+      destinationId: "BAGARMOSSEN",
+      subwayStations: subwayStations,
+      request: "patch",
+    };
+
+    const result = validateStations(data);
+    expect(result).toEqual({
+      message: "the stations needs to be located after eachother",
+    });
+  });
+});
+
+describe("Calculate travelTime", () => {
+  test("travel between bagarmossen and kärrtorp should take 2 min", async () => {
+    const app = createApp();
+
+    const result = await request(app).get(
+      "/api/stations/from/bagarmossen/to/kärrtorp"
+    );
+    const { text } = result;
+    console.log(text);
+    expect(parseInt(text)).toEqual(2);
+  });
+  test("travel between bagarmossen and slussen should take 16 min", async () => {
+    const app = createApp();
+
+    const result = await request(app).get(
+      "/api/stations/from/bagarmossen/to/slussen"
+    );
+    const { text } = result;
+    console.log(text);
+    expect(parseInt(text)).toEqual(16);
   });
 });
