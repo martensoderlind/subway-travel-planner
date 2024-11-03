e079fa32432249ac88e15acf608d0adc36a3a022
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-03 10:27:00 +0100

    fixed problem from new the new data in mockdb

--- src/features/stations/service.ts
+++ src/features/stations/service.ts
@@ -5,7 +5,7 @@ export function serviceFactory(subwayStations: Station[]) {
   return {
     getAll: async () => {
       const stations = subwayStations.map((station) => {
-        return station.namn;
+        return station.name;
       });
 
       return stations;
