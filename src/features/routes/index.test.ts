cbd3e69309bb12dc9e0e21ab1064eeda17a7415d
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-03 20:12:23 +0100

    test for delete route

--- src/features/routes/index.test.ts
+++ src/features/routes/index.test.ts
@@ -23,4 +23,25 @@ describe("Favourite routes", () => {
       ])
     );
   });
+  test("delete should make the db shorter", async () => {
+    const app = createApp();
+
+    const response = await request(app)
+      .get("/api/routes/")
+      .expect(200)
+      .expect("Content-Type", /json/);
+
+    expect(response.body).toHaveLength(1);
+
+    const deleteResponse = await request(app)
+      .delete("/api/routes/b922b09e-09e6-4bb8-a6a2-3b80197188ab")
+      .expect(200);
+
+    const response2 = await request(app)
+      .get("/api/routes/")
+      .expect(200)
+      .expect("Content-Type", /json/);
+
+    expect(response2.body).toHaveLength(0);
+  });
 });
